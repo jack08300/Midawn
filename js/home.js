9ea0989f965bbf6d6d29bf32b28cbfb9da5a18d0
brianchen501 <brianchen501@gmail.com>
2013-11-14 17:03:38 -0500

    Home AboutUs set pics

--- js/home.js
+++ js/home.js
@@ -1,5 +1,47 @@
-/*(function(){
-alert("ss");
-})();
-*/
+$(document).ready(function(){
+	setAboutUs();
+	
+});
+//Set About Us with crew pictures
+function setAboutUs(){
+	$("#wei")
+		.mouseover(function(){
+			overAboutUs(this,"pics/crews/W.png");
+		})
+		.mouseout(function(){
+			outAboutUs(this);
+		});
+	$("#jay")
+		.mouseover(function(){
+			overAboutUs(this,"pics/crews/J.png");
+		})
+		.mouseout(function(){
+			outAboutUs(this);
+		});
+	$("#iris")
+		.mouseover(function(){
+			overAboutUs(this,"pics/crews/I.png");
+		})
+		.mouseout(function(){
+			outAboutUs(this);
+		});
+	$("#brian")
+		.mouseover(function(){
+			overAboutUs(this,"pics/crews/B.png");
+		})
+		.mouseout(function(){
+			outAboutUs(this);
+		});
+}
 
+function overAboutUs(crewID, crewURL){
+	var cid = $(crewID);
+	$("#aboutUs").css('backgroundImage','url('+ crewURL +')').css('backgroundSize','100%'); //url trick noted
+	cid.css('backgroundColor','#fbcc02');
+}
+
+function outAboutUs(crewID){
+	var cid = $(crewID);
+	$("#aboutUs").css('backgroundImage','');
+	cid.css('backgroundColor','black');
+}
