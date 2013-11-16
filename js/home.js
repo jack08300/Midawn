86b6e839cb911e20ba389dd20c0c38849e02268e
brianchen501 <brianchen501@gmail.com>
2013-11-15 19:01:39 -0500

    gallery modify 1

--- js/home.js
+++ js/home.js
@@ -1,47 +1,59 @@
 $(document).ready(function(){
 	setAboutUs();
-	
+	setGallery();
+	$(".galleryPics").click(function(){centerGallery(this);});
 });
+
 //Set About Us with crew pictures
 function setAboutUs(){
-	$("#wei")
+	hoverAboutUs("#wei","pics/crews/W.png");
+	hoverAboutUs("#jay","pics/crews/J.png");
+	hoverAboutUs("#iris","pics/crews/I.png");
+	hoverAboutUs("#brian","pics/crews/B.png");
+}
+function hoverAboutUs(crewID, crewURL){
+	var cid = $(crewID);
+	var target = $("#aboutUs");
+	cid
 		.mouseover(function(){
-			overAboutUs(this,"pics/crews/W.png");
+				target.css('backgroundImage','url(' + crewURL + ')').css('backgroundSize','100%'); //url trick noted
+				cid.css('backgroundColor','#fbcc02');
 		})
 		.mouseout(function(){
-			outAboutUs(this);
-		});
-	$("#jay")
-		.mouseover(function(){
-			overAboutUs(this,"pics/crews/J.png");
-		})
-		.mouseout(function(){
-			outAboutUs(this);
-		});
-	$("#iris")
-		.mouseover(function(){
-			overAboutUs(this,"pics/crews/I.png");
-		})
-		.mouseout(function(){
-			outAboutUs(this);
-		});
-	$("#brian")
-		.mouseover(function(){
-			overAboutUs(this,"pics/crews/B.png");
-		})
-		.mouseout(function(){
-			outAboutUs(this);
+				target.css('backgroundImage','');
+				cid.css('backgroundColor','black');
 		});
 }
 
-function overAboutUs(crewID, crewURL){
-	var cid = $(crewID);
-	$("#aboutUs").css('backgroundImage','url('+ crewURL +')').css('backgroundSize','100%'); //url trick noted
-	cid.css('backgroundColor','#fbcc02');
+//set gallery label and pictures
+function setGallery(){
+	//centerGallery("#galleryPic_1");
+	hoverGallery("#galleryLabel_1","#galleryPic_1");
+	hoverGallery("#galleryLabel_2","#galleryPic_2");
+	hoverGallery("#galleryLabel_3","#galleryPic_3");
+	hoverGallery("#galleryLabel_4","#galleryPic_4");
+	hoverGallery("#galleryLabel_5","#galleryPic_5");
 }
-
-function outAboutUs(crewID){
-	var cid = $(crewID);
-	$("#aboutUs").css('backgroundImage','');
-	cid.css('backgroundColor','black');
+function hoverGallery(labelID,galleryID){
+	var label = $(labelID);
+	label
+		.mouseover(function(){
+			$('#galleryTruck').stop();
+			$('#galleryLabel').children('.labels').css('backgroundColor','#fbcc02');
+			label.css('backgroundColor','black');
+			centerGallery(galleryID);
+		});
+}
+function centerGallery(galleryPicID) {
+	var picID = $(galleryPicID);
+	var picWidth = picID.width();
+	var truckID = $("#galleryTruck");
+	var winID = $("#galleryWindow");
+	var winWidth = winID.width();
+	//try absolute position way "offset().left"
+	var picCenter = picID.offset().left+(picWidth/2);
+	var winCenter = winID.offset().left+(winWidth/2);
+	var distance = picCenter - winCenter;
+	var toGoPos = parseInt(truckID.css('marginLeft')) - distance;
+	truckID.animate({marginLeft:toGoPos+'px'},500); //try to make it move to "position" not by distance
 }
