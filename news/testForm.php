9fedd49b3d7204aa7e712e78754ba72d1aebd19a
brianchen501 <brianchen501@gmail.com>
2014-01-04 00:30:34 -0500

    checkpoint

---
+++ news/testForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>test form</title>
+</head>
+<body>
+	<form action="testPost.php" method="post">
+		Title: <input type="text" name="title"><br>
+		Author: <input type="text" name="author"><br>
+		Content: <br><textarea name="content"></textarea><br>
+		<input type="submit" value="SUBMIT">
+	</form>
+</body>
+</html>
