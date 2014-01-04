4e150ce992b95846e7750589253abcd88cc1fb18
Brian <brianchen501@gmail.com>
2014-01-04 18:25:25 -0500

    checkpoint

--- news/testForm.php
+++ news/testForm.php
@@ -7,8 +7,18 @@
 <body>
 	<form action="testPost.php" method="post">
 		Title: <input type="text" name="title"><br>
-		Author: <input type="text" name="author"><br>
-		Content: <br><textarea name="content"></textarea><br>
+		Author: 
+		<select type="text" name="author">
+			<option value="inwolf">inwolf</option>
+			<option value="Bri'an C">Bri'an C</option>
+			<option value="Raisive Crowx">Raisive Crowx</option>
+		</select><br>
+		Content: <br><textarea style="width:300px;height:500px;" name="content"></textarea><br>
+		Category: 
+		<select type="text" name="category">
+			<option value="Company">Company</option>
+			<option value="Other">Other</option>
+		</select><br>
 		<input type="submit" value="SUBMIT">
 	</form>
 </body>
